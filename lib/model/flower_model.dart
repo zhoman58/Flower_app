af09c1ab125155a097ccd512f865182005b7cc99
User <mdzhoman@gmail.com>
2022-11-12 10:35:59 +0600

    flower

---
+++ lib/model/flower_model.dart
@@ -0,0 +1,9 @@
+class FlowerModel {
+  late String img;
+  late String name;
+  late String origin;
+  late String details;
+  late String color;
+
+  FlowerModel(this.img, this.name, this.origin, this.details, this.color);
+}
