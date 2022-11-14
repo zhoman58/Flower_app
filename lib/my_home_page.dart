292d2f9f14e5915673d6f25e04c31978be8af7eb
User <mdzhoman@gmail.com>
2022-11-15 00:24:57 +0600

    done

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,25 +1,6 @@
 import 'package:flower_details/FlowerDetailsPage.dart';
+import 'package:flower_details/model/flower_modelList.dart';
 import 'package:flutter/material.dart';
-
-List<String> flowerNameList = [
-  "Rose",
-  "Marigold",
-  "Water Lily",
-  "Sunflower",
-  "Lotus",
-  "Tulip",
-  "China rose"
-];
-
-List<String> flowerImageList = [
-  "assets/image/image1.jpg",
-  "assets/image/image2.jpg",
-  "assets/image/image3.jpg",
-  "assets/image/image4.jpg",
-  "assets/image/image5.jpg",
-  "assets/image/image6.jpg",
-  "assets/image/image7.jpg",
-];
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -27,6 +8,52 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
+List<FlowerModdel> flowerMdList = [
+  FlowerModdel(
+      "assets/image/image1.jpg",
+      "Rose",
+      "Asia",
+      "গোলাপ হল ''রোজেই'' পরিবারের রোসা গণের এক প্রকারের বহুবর্ষজীবী ফুলের গাছ।[১] এখানে তিন শতাধিক প্রজাতি এবং কয়েক হাজার হাজার জাত রয়েছে।[১] এগুলি এমন এক ধরণের গাছপালা গঠন করে যা ডালপালা খাড়া করে উঠতে বা পিছনে যেতে পারে, ডালপালাগুলির সাথে প্রায়শই তীক্ষ্ন কাঁটা সজ্জিত থাকে।[১] ফুল আকার এবং আকারে পৃথক হয় এবং সাধারণত বড় এবং শোভাকর হয়, সাদা থেকে হলদে এবং লাল রঙের হয়ে থাকে",
+      "Red"),
+  FlowerModdel(
+      "assets/image/image2.jpg",
+      "Marigold",
+      "Asia",
+      "গোলাপ হল ''রোজেই'' পরিবারের রোসা গণের এক প্রকারের বহুবর্ষজীবী ফুলের গাছ।[১] এখানে তিন শতাধিক প্রজাতি এবং কয়েক হাজার হাজার জাত রয়েছে।[১] এগুলি এমন এক ধরণের গাছপালা গঠন করে যা ডালপালা খাড়া করে উঠতে বা পিছনে যেতে পারে, ডালপালাগুলির সাথে প্রায়শই তীক্ষ্ন কাঁটা সজ্জিত থাকে।[১]"
+          " ফুল আকার এবং আকারে পৃথক হয় এবং সাধারণত বড় এবং শোভাকর হয়, সাদা থেকে হলদে এবং লাল রঙের হয়ে থাকে",
+      "Red"),
+  FlowerModdel(
+      "assets/image/image3.jpg",
+      "Water Lily",
+      "Asia",
+      "গোলাপ হল ''রোজেই'' পরিবারের রোসা গণের এক প্রকারের বহুবর্ষজীবী ফুলের গাছ।[১] এখানে তিন শতাধিক প্রজাতি এবং কয়েক হাজার হাজার জাত রয়েছে।[১] এগুলি এমন এক ধরণের গাছপালা গঠন করে যা ডালপালা খাড়া করে উঠতে বা পিছনে যেতে পারে, ডালপালাগুলির সাথে প্রায়শই তীক্ষ্ন কাঁটা সজ্জিত থাকে।[১] ফুল আকার এবং আকারে পৃথক হয় এবং সাধারণত বড় এবং শোভাকর হয়, সাদা থেকে হলদে এবং লাল রঙের হয়ে থাকে",
+      "Red"),
+  FlowerModdel(
+      "assets/image/image4.jpg",
+      "Sunflower",
+      "Asia",
+      "গোলাপ হল ''রোজেই'' পরিবারের রোসা গণের এক প্রকারের বহুবর্ষজীবী ফুলের গাছ।[১] এখানে তিন শতাধিক প্রজাতি এবং কয়েক হাজার হাজার জাত রয়েছে।[১] এগুলি এমন এক ধরণের গাছপালা গঠন করে যা ডালপালা খাড়া করে উঠতে বা পিছনে যেতে পারে, ডালপালাগুলির সাথে প্রায়শই তীক্ষ্ন কাঁটা সজ্জিত থাকে।[১] ফুল আকার এবং আকারে পৃথক হয় এবং সাধারণত বড় এবং শোভাকর হয়, সাদা থেকে হলদে এবং লাল রঙের হয়ে থাকে",
+      "Red"),
+  FlowerModdel(
+      "assets/image/image5.jpg",
+      "Lotus",
+      "Asia",
+      "গোলাপ হল ''রোজেই'' পরিবারের রোসা গণের এক প্রকারের বহুবর্ষজীবী ফুলের গাছ।[১] এখানে তিন শতাধিক প্রজাতি এবং কয়েক হাজার হাজার জাত রয়েছে।[১] এগুলি এমন এক ধরণের গাছপালা গঠন করে যা ডালপালা খাড়া করে উঠতে বা পিছনে যেতে পারে, ডালপালাগুলির সাথে প্রায়শই তীক্ষ্ন কাঁটা সজ্জিত থাকে।[১] ফুল আকার এবং আকারে পৃথক হয় এবং সাধারণত বড় এবং শোভাকর হয়, সাদা থেকে হলদে এবং লাল রঙের হয়ে থাকে",
+      "Red"),
+  FlowerModdel(
+      "assets/image/image6.jpg",
+      "Tulip",
+      "Asia",
+      "গোলাপ হল ''রোজেই'' পরিবারের রোসা গণের এক প্রকারের বহুবর্ষজীবী ফুলের গাছ।[১] এখানে তিন শতাধিক প্রজাতি এবং কয়েক হাজার হাজার জাত রয়েছে।[১] এগুলি এমন এক ধরণের গাছপালা গঠন করে যা ডালপালা খাড়া করে উঠতে বা পিছনে যেতে পারে, ডালপালাগুলির সাথে প্রায়শই তীক্ষ্ন কাঁটা সজ্জিত থাকে।[১] ফুল আকার এবং আকারে পৃথক হয় এবং সাধারণত বড় এবং শোভাকর হয়, সাদা থেকে হলদে এবং লাল রঙের হয়ে থাকে",
+      "Red"),
+  FlowerModdel(
+      "assets/image/image7.jpg",
+      "China rose",
+      "Asia",
+      "গোলাপ হল ''রোজেই'' পরিবারের রোসা গণের এক প্রকারের বহুবর্ষজীবী ফুলের গাছ।[১] এখানে তিন শতাধিক প্রজাতি এবং কয়েক হাজার হাজার জাত রয়েছে।[১] এগুলি এমন এক ধরণের গাছপালা গঠন করে যা ডালপালা খাড়া করে উঠতে বা পিছনে যেতে পারে, ডালপালাগুলির সাথে প্রায়শই তীক্ষ্ন কাঁটা সজ্জিত থাকে।[১] ফুল আকার এবং আকারে পৃথক হয় এবং সাধারণত বড় এবং শোভাকর হয়, সাদা থেকে হলদে এবং লাল রঙের হয়ে থাকে",
+      "Red"),
+];
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
@@ -41,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           Expanded(
             child: ListView.builder(
-                itemCount: flowerNameList.length,
+                itemCount: flowerMdList.length,
                 itemBuilder: (context, index) {
                   return Padding(
                     padding:
@@ -56,21 +83,35 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                             color: Colors.teal),
                         child: Center(
-                            child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => FlowerDetailsPage(
-                                          name: flowerNameList[index],
-                                          img: flowerImageList[index],
-                                        )));
-                          },
-                          child: Text(
-                            flowerNameList[index],
-                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => FlowerDetails(
+                                            flowerModdel: flowerMdList[index],
+                                          )));
+                            },
+                            child: Text(
+                              flowerMdList[index].name,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+
+                            // // decoration: BoxDecoration(
+                            // //   border: Border.all(
+                            // //     width: 8,
+                            // //     color: Colors.yellow,
+                            //   ),
+                            // ),
+                            // height: 200,
+                            // width: 200,
+                            // child: Image.asset(
+                            //   flowerMdList[index].img,
+                            //   fit: BoxFit.cover,
+                            // ),
                           ),
-                        ))),
+                        )),
                   );
                 }),
           )
