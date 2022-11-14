b493cc317c4ff4acb8e0ee6198259b2c03fe9ad9
User <mdzhoman@gmail.com>
2022-11-15 00:24:57 +0600

    flowerapps with model

--- lib/FlowerDetailsPage.dart
+++ lib/FlowerDetailsPage.dart
@@ -1,36 +1,110 @@
+import 'package:flower_details/model/flower_modelList.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FlowerDetailsPage extends StatefulWidget {
-  String name, img;
-  FlowerDetailsPage({Key? key, required this.name, required this.img})
-      : super(key: key);
+class FlowerDetails extends StatefulWidget {
+  FlowerModdel flowerModdel;
+  FlowerDetails({Key? key, required this.flowerModdel}) : super(key: key);
 
   @override
-  State<FlowerDetailsPage> createState() => _FlowerDetailsPageState();
+  State<FlowerDetails> createState() => _FlowerDetailsState();
 }
 
-class _FlowerDetailsPageState extends State<FlowerDetailsPage> {
+class _FlowerDetailsState extends State<FlowerDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.name),
+        title: Text(widget.flowerModdel.name),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
-            child: Column(
-          children: [
-            Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 8, color: Colors.teal),
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 8, color: Colors.teal),
+                  ),
+                  height: 300,
+                  width: 300,
+                  child: Image.asset(widget.flowerModdel.img)),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Name:    ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(widget.flowerModdel.name),
+                  ],
                 ),
-                height: 300,
-                width: 300,
-                child: Image.asset(widget.img)),
-            Text(widget.name),
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Origin:    ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(widget.flowerModdel.origin),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Color:    ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(widget.flowerModdel.color),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Details:    ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        widget.flowerModdel.details,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         )),
       ),
     );
---
+++ lib/model/flower_modelList.dart
@@ -0,0 +1,9 @@
+class FlowerModdel {
+  late String img;
+  late String name;
+  late String origin;
+  late String details;
+  late String color;
+
+  FlowerModdel(this.img, this.name, this.origin, this.details, this.color);
+}
